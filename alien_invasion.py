0e529d0817fd90f8fe88613919ac4ab4dc9f0dc5
hagikehappy <hagikehappy@163.com>
2023-10-03 17:09:17 +0800

    After Config

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -9,9 +11,11 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings().config
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((self.settings['screen_width'], self.settings['screen_height']))
+        pygame.display.set_caption(self.settings['caption'])
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -21,8 +25,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # 绘制图形
+            # 画图
+            self.screen.fill(self.settings['bg_color'])
+
+            # 刷新显示
             pygame.display.flip()
+            self.clock.tick(self.settings['refresh_rate'])
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import json
+
+
+class Settings:
+    """设置类"""
+
+    def __init__(self):
+        """初始化游戏设置"""
+        self.config_path = Path('config.json')
+        self.config = {}
+
+        if self.config_path.exists():
+            with open(self.config_path, "r") as f:
+                self.config = json.load(f)
+        else:
+            self.config['screen_width'] = 1920
+            self.config['screen_height'] = 1080
+            self.config['bg_color'] = (230, 230, 230)
+            self.config['refresh_rate'] = 240
+            self.config['caption'] = "Alien Invasion"
+
+            with open(self.config_path, "w") as f:
+                json.dump(self.config, f)
+
+
